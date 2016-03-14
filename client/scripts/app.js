ba8e37ec0a6261fdac7bb7f3b5dfea8229b50f61
alexnitta <a.j.nitta@gmail.com>
2016-03-14 15:33:36 -0700

    Implement basic XSS escaping

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,21 @@
 // YOUR CODE HERE:
+var escaping = function (userInput) {
+  // create new variable 
+ // loop through userInput to check for special harmful characters
+   // if characters are safe then add to result
+ 
+ //return result outside loop 
+ 
+
+  var results = '';
+  var maliciousTypes = /[^a-zA-Z0-9\s\\.\\!\\,\\?]/gi;
+  for (var i = 0; i < userInput.length; i++) {
+    if (!userInput[i].match(maliciousTypes)) {
+      results += userInput[i];
+    }
+  }
+  return results;
+
+};
+
 
